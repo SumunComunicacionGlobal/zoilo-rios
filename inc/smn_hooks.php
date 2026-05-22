fab219b9d9103e30d6a63abd56b95bf42f470b23
Fernando Blasco <fernando@sumun.net>
2026-05-22 16:30:57 +0200

    add child pages after content

--- inc/smn_hooks.php
+++ inc/smn_hooks.php
@@ -198,23 +198,18 @@ add_filter('get_custom_logo', function($html) {
 
 add_filter( 'the_content', 'smn_append_child_pages_after_content', 20 );
 function smn_append_child_pages_after_content( $content ) {
+
     if ( ! is_singular() || is_admin() || ! in_the_loop() || ! is_main_query() ) {
         return $content;
     }
 
-    if ( strpos( home_url(), 'sumun.dev' ) === false ) {
-        return $content;
-    }
-
-    $post_id = get_the_ID();
-    if ( ! $post_id ) {
-        return $content;
-    }
-
-    $children = get_pages( [
-        'parent'      => $post_id,
-        'sort_column' => 'menu_order,post_title',
-        'sort_order'  => 'ASC',
+    global $post;
+    
+    $children = get_posts( [
+        'post_parent' => $post->ID,
+        'post_type'   => $post->post_type,
+        'orderby'     => 'menu_order title',
+        'order'       => 'ASC',
         'post_status' => 'publish',
     ] );
 
@@ -222,16 +217,18 @@ function smn_append_child_pages_after_content( $content ) {
         return $content;
     }
     $output = '';
-    $output .= '<h2 class="smn-child-pages-title">' . esc_html__( '¿Quieres saber más?', 'zoilo' ) . '</h2>';
-    $output .= '<ul class="smn-child-pages">';
-    foreach ( $children as $child ) {
-        $output .= sprintf(
-            '<li><a href="%s">%s</a></li>',
-            esc_url( get_permalink( $child->ID ) ),
-            esc_html( get_the_title( $child->ID ) )
-        );
-    }
-    $output .= '</ul>';
+    $output .= '<div class="smn-child-pages-wrapper">';
+        $output .= '<h2 class="smn-child-pages-title has-heading-3-font-size">' . esc_html__( '¿Quieres saber más?', 'zoilo' ) . '</h2>';
+        $output .= '<ul class="smn-child-pages wp-block-list is-style-arrow-mini-separator-list">';
+        foreach ( $children as $child ) {
+            $output .= sprintf(
+                '<li><a href="%s">%s</a></li>',
+                esc_url( get_permalink( $child->ID ) ),
+                esc_html( get_the_title( $child->ID ) )
+            );
+        }
+        $output .= '</ul>';
+    $output .= '</div>';
 
     return $content . $output;
 }
